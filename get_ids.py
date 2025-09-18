2861c3645705b847ab08b347445016a054f4da03
Piranha <90665330+piranhap@users.noreply.github.com>
2025-09-18 09:56:47 -0600

    Update get_ids.py

--- get_ids.py
+++ get_ids.py
@@ -3,8 +3,12 @@ import json
 
 
 url = "https://businer.com/fakeid.php?gender=r"
+r.encoding = 'ISO-8859-1'
 
-response = requests.get(url=url)
+r = requests.get(url=url)
+r.text
+
+
 # print(response.headers) # Type is text/html, charset iso-8859-1
 
 
